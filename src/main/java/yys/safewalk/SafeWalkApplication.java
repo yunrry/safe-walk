1e13274ff825109a3eaed1c658d0b8da0d4585b0
yunrry <yunryy329@gmail.com>
2025-08-13 11:46:45 +0900

    springboot settings

---
+++ src/main/java/yys/safewalk/SafeWalkApplication.java
@@ -0,0 +1,13 @@
+package yys.safewalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeWalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeWalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yys/safewalk/SafeWalkApplicationTests.java
@@ -0,0 +1,13 @@
+package yys.safewalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeWalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
