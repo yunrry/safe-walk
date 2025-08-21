3cbbfadf89be972706fb180cb16777565995dacf
yunrry <yunryy329@gmail.com>
2025-08-21 14:08:21 +0900

    fix: springdoc

--- src/main/java/yys/safewalk/infrastructure/config/SwaggerConfig.java
+++ src/main/java/yys/safewalk/infrastructure/config/SwaggerConfig.java
@@ -6,8 +6,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -25,6 +28,10 @@ public class SwaggerConfig {
                 .components(new Components()
                         .addSecuritySchemes("noauth", new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
-                                .scheme("basic")));
+                                .scheme("basic")))
+                .servers(List.of(
+                        new Server().url("https://api.safewalk.cloud/api").description("Production"),
+                        new Server().url("http://localhost:8080/api").description("Local")
+                ));
     }
 }
